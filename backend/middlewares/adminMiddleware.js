20ce59fdaa6b4890522a8b7ae467dfcf98603043
syedalihax <Syedalihax@gmail.com>
2026-08-29 18:16:45 +0500

    fix the error handling

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -3,22 +3,22 @@ const UserModel = require("../Model/userModel")
 const authorizeAdmin = async (req, res, next) => {
   try {
     const user = await UserModel.findById(req.user.id);
-  
+    if (user && user.role === "admin") {
+      next();
+
+    } else {
+      return res.status(403).json({
+        success: false,
+        message: "Forbidden: Admin access required",
+      });
+    }
   } catch (error) {
     return res.status(500).json({
-      success:false,
-      message:error.message
-    })  
-  }
-  if (user && user.role === "admin") {
-    next(); 
-    
-  } else {
-    return res.status(403).json({
       success: false,
-      message: "Forbidden: Admin access required",
-    });
+      message: error.message
+    })
   }
+
 };
 
 module.exports = authorizeAdmin
