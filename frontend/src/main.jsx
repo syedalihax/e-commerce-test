740bfc53ac13cdf86fab6b6fd7e449a6950415a7
farooqzehri <farooqahmedbaloch17@gmail.com>
2026-08-29 17:33:00 +0500

    main jsx hogaya abhii

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Home from './pages/Home.jsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Home />
-  </StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  </BrowserRouter>
 )
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Login() {
+  const [email , setEmail] = useState('')
+  const [password , setPassword] = useState('')
+  const LoginUser = (e) => {
+    e.preventDefault()
+    console.log(email , password);
+    
+  }
   return (
-    <div>Login</div>
+    <>
+  <div>
+      <form>
+      <input type="email" placeholder='Enter Your Email' value={email} onChange={(e)=>setEmail(e.target.value)} />
+      <input type="password"  placeholder='Enter Your Password' value={password} onChange={(e)=>setPassword(e.target.value)}/>
+      <button type='submit'>Login</button>
+    </form>
+  </div>
+    </>
   )
 }
 
